de2f56ecd06872a878af2c33218120207757faaf
Guilherme Carvalhal Alves <149338283+GuilhermeCarvalhal@users.noreply.github.com>
2024-03-20 12:34:44 -0300

    Exercícios

---
+++ Tarefas/Programação em Computadores/Exercício 2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String nome, sexo, estado;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual seu nome?");
+
+        nome = sc.nextLine();
+
+        System.out.println("Você está casado(a)? (Y/N)");
+
+        estado = sc.nextLine();
+
+        System.out.println("Qual seu sexo?");
+        System.out.println("'F' para Mulher e 'M' para homem" );
+
+        sexo = sc.nextLine();
+
+        if ((estado.equals("Y")) && (sexo.equals("F")))
+        {
+            System.out.println("Há quanto tempo você está casada?");
+
+            int Casada = sc.nextInt();
+
+            System.out.printf("Você é uma mulher casada há %d", Casada);
+        }
+        else
+        {
+            System.out.println("Obrigado!");;
+        }
+
+        sc.close();
+
+    }
+}
