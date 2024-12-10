3c672cf95545d8202b39414e33f759895a81ce3f
GuilhermeCarvalhal <guilherme.carvalhal.alves@gmail.com>
2024-12-09 21:14:37 -0300

    Teste

---
+++ Atividades/Trabalho Estrutura de Dados 2/src/ArvoreAVL.java
@@ -0,0 +1,35 @@
+public class ArvoreAVL {
+    
+    private NodoArvoreAVL raiz;
+    
+    ArvoreAVL(){
+        raiz = null;
+    }
+
+    int altura(){
+        if (raiz == null) return -1;
+        else return raiz.altura(raiz);
+    
+    }
+    
+    void insere(Integer v){
+        if (this.raiz == null) 
+            raiz = new NodoArvoreAVL(v, null, null);
+        else
+            raiz = raiz.insereBalanceado(raiz, v);
+    }
+    
+    void imprimeSim(){
+        if (raiz != null){
+            raiz.imprimeSim(raiz);
+        }
+        System.out.println();
+    
+    }
+    
+   // void retira(Integer v){
+    //    if (this.raiz != null)
+    //       raiz = raiz.retiraValor(raiz, v);
+   // }
+    
+}
